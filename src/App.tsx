d1487ab50b666396d10e0f7429ab39da8ca3dc35
David Mayes <davidrmayes@gmail.com>
2019-05-19 17:22:35 -0600

    Added top nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { 
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom';
+import Home from './components/home/home'
+import TopNav from './components/top-nav/top-nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            // <div className="App">
+            //     <header className="App-header">
+            //     </header>
+            // </div>
+            <Router>
+                <TopNav />
+                <div>
+                    <Route exact path="/" component={Home}/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
