e443379400a94eeddae5d8eb4df963822e99d840
David Mayes <davidrmayes@gmail.com>
2019-05-27 21:36:20 -0600

    Starting tabs from scratch

--- src/components/recipe-filter/recipe-filter.tsx
+++ src/components/recipe-filter/recipe-filter.tsx
@@ -29,52 +29,6 @@ class RecipeFilter extends Component<TestProps> {
 
 }
 
-const useStyles = makeStyles((theme: Theme) => ({
-    root: {
-        flexGrow: 1,
-        backgroundColor: theme.palette.background.paper,
-    },
-}));
-
-interface TabContainerProps {
-    children?: React.ReactNode;
-}
-
-function TabContainer(props: TabContainerProps) {
-    return (
-        <Typography component="div" style={{ padding: 8 * 3 }}>
-            {props.children}
-        </Typography>
-    );
-}
-
-TabContainer.propTypes = {
-    children: PropTypes.node.isRequired,
-}
-
-function SimpleTabs() {
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
-
-    function handleChange(event: React.ChangeEvent<{}>, newValue: number) {
-        setValue(newValue);
-    }
-
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Tabs value={value} onChange={handleChange}>
-                    <Tab label="Item One" />
-                    <Tab label="Item Two" />
-                    <Tab label="Item Three" />
-                </Tabs>
-            </AppBar>
-            {value === 0 && <TabContainer>Item One</TabContainer>}
-            {value === 1 && <TabContainer>Item Two</TabContainer>}
-            {value === 2 && <TabContainer>Item Three</TabContainer>}
-        </div>
-    );
-}
 const mapStateToProps = (recipes: RecipeState) => {
     console.log(recipes);
     return recipes;
